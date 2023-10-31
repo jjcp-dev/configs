86678b3e7dd243321ea230c71dee9b6e09cdf753
Juan Cabrera <juan@jjcp.dev>
2023-10-31 15:34:23 -0300

    Initial nvim config

---
+++ nvim/init.lua
@@ -0,0 +1,49 @@
+require("base")
+
+require("lazy-bootstrap")
+require("lazy").setup({
+    -- Colorschemes
+    {
+        "dustypomerleau/tol.nvim",
+        lazy = false, -- load the colorscheme at startup
+        priority = 1000, -- load colorscheme first
+        config = true,
+    },
+    ---------------------------------------
+    {
+        "nvim-telescope/telescope.nvim", tag = '0.1.4',
+        dependencies = { "nvim-lua/plenary.nvim" }
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+    },
+    { "mbbill/undotree" },
+    { "lewis6991/gitsigns.nvim" },
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+          "nvim-lua/plenary.nvim",
+          "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+          "MunifTanjim/nui.nvim",
+          -- "3rd/image.nvim", -- Optional image support in preview window: 
+	                       -- See `# Preview Mode` for more information
+        }
+    },
+    -- LSP
+    {"williamboman/mason.nvim"},
+    {"williamboman/mason-lspconfig.nvim"},
+    {"VonHeikemen/lsp-zero.nvim", branch = "v3.x"},
+    {"neovim/nvim-lspconfig"},
+    {"hrsh7th/cmp-nvim-lsp"},
+    {"hrsh7th/nvim-cmp"},
+    {"L3MON4D3/LuaSnip"},
+    ---------------------------------------
+})
+
+
+---------------------------------------
+
+
+vim.cmd[[colorscheme tol]]
---
+++ nvim/lua/base/init.lua
@@ -0,0 +1,8 @@
+vim.g.mapleader = " "
+
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+vim.opt.relativenumber = true
